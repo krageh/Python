129f7fa21df0b7b3e7b941f17bd250895b51febd
krageh <krageh@mun.ca>
2020-11-11 13:08:40 -0700

    Submitted CF code for A-431

---
+++ Code Forces/A - Black Square - 431.py
@@ -0,0 +1,60 @@
+# a = [int(x) for x in input().split()]
+# s = input()
+
+# total_cal = []
+
+# for i in range(len(s)):
+#     if s[i] == '1':
+#         total_cal.append(a[0])
+#     elif s[i] == '2':
+#         total_cal.append(a[1])
+#     elif s[i] == '3':
+#         total_cal.append(a[2])
+#     elif s[i] == '4':
+#         total_cal.append(a[3])
+# print (sum(total_cal))
+
+# a = [int(x) for x in input().split()]
+# s = input()
+# s_l = list(s)
+
+# for i in range(len(s)):
+#     s_l[i] = int(s_l[i])
+
+# l = []
+
+# for i in range(len(s)):
+#     l.append(a[s_l[i] - 1])
+    
+# print (sum(l))
+
+################################3
+# a = [int(x) for x in input().split()]
+# s = input()
+
+# x = 0
+
+# for i in range(len(s)):
+#     if s[i] == '1':
+#         x += a[0]
+#     elif s[i] == '2':
+#         x += a[1]
+#     elif s[i] == '3':
+#         x += a[2]
+#     elif s[i] == '4':
+#         x += a[3]
+# print (x)
+
+# a = [int(x) for x in input().split()]
+# s = input()
+# s_l = list(s)
+
+# for i in range(len(s)):
+#     s_l[i] = int(s_l[i])
+
+# x = 0
+
+# for i in range(len(s)):
+#     x += a[s_l[i] - 1]
+    
+# print (x)

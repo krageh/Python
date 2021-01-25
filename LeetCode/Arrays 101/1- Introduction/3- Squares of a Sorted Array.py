3aeaa088a2e70b5c82963ab43652bbb87b6dc270
krageh <krageh@mun.ca>
2021-01-25 15:09:01 -0700

    Uploaded Intro (3 0f 3)

---
+++ LeetCode/Arrays 101/1- Introduction/3- Squares of a Sorted Array.py
@@ -0,0 +1,14 @@
+nums = [-4,-1,0,3,10]
+# for i in range(len(nums)):
+#     nums[i] = pow(nums[i], 2)
+# nums.sort()
+# print(nums)
+
+# compare operations
+
+def square(n):
+    return n*n
+
+result = list(map(square, nums))
+result.sort()
+print(result)

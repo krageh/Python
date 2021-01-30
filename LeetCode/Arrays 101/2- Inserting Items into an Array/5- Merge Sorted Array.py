f42003ad024c704e0c5d61d426fedb955561d8cf
krageh <krageh@mun.ca>
2021-01-29 17:52:41 -0700

    Uploaded Insert (2 of 2)

---
+++ LeetCode/Arrays 101/2- Inserting Items into an Array/5- Merge Sorted Array.py
@@ -0,0 +1,20 @@
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+m = 3
+n = 3
+
+# for i in range(n):
+#     nums1.pop()
+# nums1.extend(nums2)
+# nums1.sort()
+# print(nums1)
+
+# nums1 = sorted(nums1[:m] + nums2)
+# # nums1[:] = sorted(nums1[:m] + nums2)
+# print(nums1)
+
+# for i in range(m, m + n):
+#     for t in range(n):
+#         nums1[i] = nums2[t]
+# print(nums1)
+

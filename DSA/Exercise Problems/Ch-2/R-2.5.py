a28f685378622a184fb35448da4aa302fa280913
krageh <krageh@mun.ca>
2021-03-09 19:44:00 -0700

    Uploaded Reinforcement 2.5

---
+++ DSA/Exercise Problems/Ch-2/R-2.5.py
@@ -0,0 +1,40 @@
+class CreditCard():
+    def __init__(self, customer, bank, acnt, limit):
+        self._customer = customer
+        self._bank = bank
+        self._account = acnt
+        self._limit = limit
+        self._balance = 0
+
+    def get_customer(self):
+        return self._customer
+
+    def get_bank(self):
+        return self._bank
+    
+    def get_account(self):
+        return self._account
+
+    def get_limit(self):
+        return self._limit
+
+    def get_balance(self):
+        return self._balance
+
+    def charge(self, price):
+        try:
+            if price + self._balance > self._limit:
+                return False
+            else:
+                return True
+        except (TypeError):
+            print('Please enter a numeric value.')
+            return False
+
+    def make_payment(self, amount):
+        try:
+            self._balance -= amount
+        except (TypeError):
+            print('Please enter a numeric value.')
+            return False
+
